b8776b2434d4bc4ca7e31d1d0c2dee24417f7ba7
khole <mail@interwebz.space>
2016-11-26 19:15:05 -0700

    Added viewport and made mobile responsive via CSS3 media queries          /*consolelog for testing the loading*/

--- js/index.js
+++ js/index.js
@@ -1,24 +1,22 @@
 $(document).ready(function() {
   var leftQuote = '<i class="fa fa-quote-left fa-4x" aria-hidden="true"></i>';
   var rightQuote = '<i class="fa fa-quote-right fa-4x" aria-hidden="true"></i>';
-  
-    
+
+
     $( "#mybutton" ).click(function() {
-  
+
        $( ".btn-info" ).toggleClass(".btn-primary").animate({ top: '85%' }, "slow");
        $( "#quotespace" ).animate( {top: '50px'}, "slow");
-       
-    
+
+
          /*This is where we grab our JSON after button press
-         
+
          Make sure to load code pen as HTTP or this call doesn't work'*/
          $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(data) {
-      
+
           /*this is where we display the quote on the id = quoteapace div the p tag. font awesome quotes icons on HTML*/
            $( "#quotespace>#quote" ).html( leftQuote + data.quoteText + rightQuote + "<br>" + "-" + data.quoteAuthor);
-      
-          /*consolelog for testing the loading*/
-            console.log(data.quoteText + "-" + data.quoteAuthor );
+
          });
     });
  });
